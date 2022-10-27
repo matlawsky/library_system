ebf1524a1c482396656c6cf395b726492d353c35
Mateusz Domański <kontakt@mateuszdomanski.pl>
2022-10-28 00:16:07 +0200

    users files

--- alexandria/users/views.py
+++ alexandria/users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+def signup(request):
+    render(request, 'users/signup.html')
+
+def login(request):
+    render(request, 'users/login.html')
+
+def logout(request):
+    # TODO  need to route to homepage
+    # and don't forget to logout
+    render(request, 'users/logout.html')
+
+def myaccount(request):
+    render(request, 'users/myaccount.html')
+
+def mybooks(request):
+    render(request, 'users/mybooks.html')
