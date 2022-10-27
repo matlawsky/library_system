75e5d0fe7baa37f2b87d46c602d0635fa0961ea9
Mateusz Domański <kontakt@mateuszdomanski.pl>
2022-10-28 00:13:46 +0200

    models for both apps

--- alexandria/books/models.py
+++ alexandria/books/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    bookId = models.PositiveIntegerField()
+    title = models.CharField(max_length=60)
+    author = models.CharField(max_length=60)
+
+class CopyBook(Book):
+    copyId = models.IntegerField()
+    _bookId = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='book',
+        )
+    availableAt = models.DateField()
+
+# class Comment():
+#     commentId = models.PositiveIntegerField()
+#     userId = models.ForeignKey()
+#     bookId = models.ForeignKey()
+#     content = models.TextField()
+
+# class Rating():
+#     pass
--- alexandria/users/models.py
+++ alexandria/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    userId = models.PositiveIntegerField()
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+
+class Reader(User):
+    availableAt = models.DateField()
+    account_balance = models.FloatField()
+
+class Worker(User):
+    pass
