91c4a10ec7f542c8f441ba5a693979bad1dbf217
Mateusz Domański <kontakt@mateuszdomanski.pl>
2022-10-30 12:03:43 +0100

    signup function added

--- alexandria/users/views.py
+++ alexandria/users/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def signup(request):
-    return render(request, 'users/signup.html')
+    if request.method == 'POST':
+        # the user wants to sign up
+
+            if request.POST.get('password') == request.POST.get('passwordCheck'):
+                try:
+                    user = User.objects.get(username=request.POST.get('userName'))
+                    return render(request, 'users/signup.html', {'error': 'Username has already been taken!'})
+                except User.DoesNotExist:
+                    user = User.objects.create_user(
+                    username=request.POST.get('userName'),
+                    password=request.POST.get('password'),
+                    email=request.POST.get('email'),
+                    first_name= request.POST.get('firstName'),
+                    last_name=request.POST.get('lastName'),
+                    )
+                    auth.login(request,user)
+                    return redirect('home')
+            else:
+                return render(request, 'users/signup.html', {'error': 'Passwords do not match!'})               
+
+    else: 
+        # user wants to enter info
+        return render(request, 'users/signup.html')
 
 def login(request):
     return render(request, 'users/login.html')
