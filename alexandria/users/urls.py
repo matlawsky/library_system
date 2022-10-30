d1ac185b9eb27e3ea1ab07ff1a7bbcdb65cb94c0
Mateusz Domański <kontakt@mateuszdomanski.pl>
2022-10-30 12:43:52 +0100

    fixing function names

--- alexandria/users/urls.py
+++ alexandria/users/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     path('signup', views.signup, name='signup'),
-    path('login', views.signup, name='login'),
-    path('logout', views.signup, name='logout'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
     path('myaccount', views.myaccount, name='myaccount'),
     path('mybooks', views.mybooks, name='mybooks'),
 ]
