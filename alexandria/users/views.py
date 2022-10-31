fdc4689af3f72976bca1854b836ea4d5e996ad9a
Mateusz Domański <kontakt@mateuszdomanski.pl>
2022-10-31 10:09:37 +0100

    adding working logout function

--- alexandria/users/views.py
+++ alexandria/users/views.py
@@ -5,35 +5,45 @@ from django.contrib import auth
 def signup(request):
     if request.method == 'POST':
         # the user wants to sign up
-
-            if request.POST.get('password') == request.POST.get('passwordCheck'):
-                try:
-                    user = User.objects.get(username=request.POST.get('userName'))
-                    return render(request, 'users/signup.html', {'error': 'Username has already been taken!'})
-                except User.DoesNotExist:
-                    user = User.objects.create_user(
-                    username=request.POST.get('userName'),
-                    password=request.POST.get('password'),
-                    email=request.POST.get('email'),
-                    first_name= request.POST.get('firstName'),
-                    last_name=request.POST.get('lastName'),
-                    )
-                    auth.login(request,user)
-                    return redirect('home')
-            else:
-                return render(request, 'users/signup.html', {'error': 'Passwords do not match!'})               
+        if request.POST.get('password') == request.POST.get('passwordCheck'):
+            try:
+                user = User.objects.get(username=request.POST.get('userName'))
+                return render(request, 'users/signup.html', {'error': 'Username has already been taken!'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(
+                username=request.POST.get('userName'),
+                password=request.POST.get('password'),
+                email=request.POST.get('email'),
+                first_name= request.POST.get('firstName'),
+                last_name=request.POST.get('lastName'),
+                )
+                auth.login(request,user)
+                return redirect('home')
+        else:
+            return render(request, 'users/signup.html', {'error': 'Passwords do not match!'})               
 
     else: 
         # user wants to enter info
         return render(request, 'users/signup.html')
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        # the user wants to sign up
+        user = auth.authenticate(username=request.POST.get('userName'), password=request.POST.get('password'))             
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else: 
+            return render(request, 'users/login.html', {'error': 'Username or password is incorrect!'})             
+    else: 
+        # user wants to enter info
+        return render(request, 'users/login.html')
 
 def logout(request):
-    # TODO  need to route to homepage
-    # and don't forget to logout
-    render(request, 'users/logout.html')
+    if request.method == 'POST':
+        # need to route to homepage
+        auth.logout(request)
+        return redirect('home')
 
 def myaccount(request):
     return render(request, 'users/myaccount.html')
