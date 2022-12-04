aeb34b29414e180560b34fbe827b1b5222836668
Mateusz Domański <kontakt@mateuszdomanski.pl>
2022-12-04 22:46:15 +0100

    cusomizing view, registering users in django admin

--- alexandria/users/admin.py
+++ alexandria/users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import ReaderCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = ReaderCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "birth_date",
+        "is_staff",
+    ]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("birth_date",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("birth_date",)}),)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
