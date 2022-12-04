3186a62a9fdb1eb84fcab18a3fe46034979f7a9b
Mateusz Domański <kontakt@mateuszdomanski.pl>
2022-12-04 22:45:29 +0100

    actually aadding working paths for book app

--- alexandria/books/urls.py
+++ alexandria/books/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from .views import MyBooksView, BooksCatalogView
 
 urlpatterns = [
-    path('addbook', views.addbook, name='addbook'),
-    path('deletebook', views.deletebook, name='deletebook'),
-    path('returnbook', views.returnbook, name='returnbook'),
-    path('borrowbook', views.borrowbook, name='borrowbook'),
-    path('requestbook', views.requestbook, name='requestbook'),
-    path('reservebook', views.reservebook, name='reservebook'),
+    path("mybooks/", MyBooksView.as_view(), name="mybooks"),
+    path("bookscatalog/", BooksCatalogView.as_view(), name="bookscatalog"),
+    path("book/", BooksCatalogView.as_view(), name="book"),
+    # path("addbook", views.addbook, name="addbook"),
+    # path("deletebook", views.deletebook, name="deletebook"),
+    # path("returnbook", views.returnbook, name="returnbook"),
+    # path("borrowbook", views.borrowbook, name="borrowbook"),
+    # path("requestbook", views.requestbook, name="requestbook"),
+    # path("reservebook", views.reservebook, name="reservebook"),
 ]
