1883219a1a2464c3c0a100cd0a90f4c916f2f72f
Mateusz Domański <kontakt@mateuszdomanski.pl>
2022-12-04 22:44:43 +0100

    extend book models to be alligned with django way

--- alexandria/books/models.py
+++ alexandria/books/models.py
@@ -1,24 +1,28 @@
 from django.db import models
+from django.urls import reverse
+
 
 class Book(models.Model):
-    bookId = models.PositiveIntegerField()
     title = models.CharField(max_length=60)
     author = models.CharField(max_length=60)
+    pages = models.PositiveSmallIntegerField()
+    description = models.TextField()
+    ISBN = models.IntegerField()
+    release_date = models.IntegerField()
 
-class CopyBook(Book):
-    copyId = models.IntegerField()
-    _bookId = models.ForeignKey(
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("book_detail", kwargs={"pk": self.pk})
+
+    def summary(self):
+        return self.description[:100]
+
+
+class CopyBook(models.Model):
+    book = models.ForeignKey(
         Book,
         on_delete=models.CASCADE,
-        related_name='book',
-        )
-    availableAt = models.DateField()
-
-# class Comment():
-#     commentId = models.PositiveIntegerField()
-#     userId = models.ForeignKey()
-#     bookId = models.ForeignKey()
-#     content = models.TextField()
-
-# class Rating():
-#     pass
+    )
+    availableAt = models.DateField(auto_now_add=True)
