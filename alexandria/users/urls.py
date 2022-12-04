9dc6c7c66a7804941f17e451e6fc423e49d6d11a
Mateusz Domański <kontakt@mateuszdomanski.pl>
2022-12-04 22:43:55 +0100

    using customized django generic views for users

--- alexandria/users/urls.py
+++ alexandria/users/urls.py
@@ -1,12 +1,48 @@
-
 from django.contrib import admin
-from django.urls import path, include
-from . import views
+from django.urls import path
+from .views import (
+    SignUpView,
+    LogInView,
+    MyAccountView,
+    CustomPasswordChangeView,
+    CustomPasswordChangedView,
+    CustomPasswordResetCompleteView,
+    CustomPasswordResetConfirmView,
+    CustomPasswordResetDoneView,
+    CustomPasswordResetView,
+)
 
 urlpatterns = [
-    path('signup', views.signup, name='signup'),
-    path('login', views.login, name='login'),
-    path('logout', views.logout, name='logout'),
-    path('myaccount', views.myaccount, name='myaccount'),
-    path('mybooks', views.mybooks, name='mybooks'),
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("login/", LogInView.as_view(), name="login"),
+    path("myaccount/", MyAccountView.as_view(), name="myaccount"),
+    path(
+        "password_change/", CustomPasswordChangeView.as_view(), name="password_change"
+    ),
+    path(
+        "password_change_done/",
+        CustomPasswordChangedView.as_view(),
+        name="password_change_done",
+    ),
+    path(
+        "password_reset/",
+        CustomPasswordResetView.as_view(),
+        name="password_reset",
+    ),
+    path(
+        "password_reset_done/",
+        CustomPasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "password_reset_complete/",
+        CustomPasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
+    path(
+        "password_reset_confirm/",
+        CustomPasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
 ]
+# path("mybooks/", MyBooksView.as_view(), name="mybooks"),
